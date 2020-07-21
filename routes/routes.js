b8a5407f79b6c22df6be3f623faf607586170df3
jeanhern81 <jeanhern81@gmail.com>
2020-07-21 14:37:44 -0700

    added notes

--- routes/routes.js
+++ routes/routes.js
@@ -4,35 +4,33 @@ const path = require("path");
 module.exports = app => {
 
     //Variables for the notes
-
     fs.readFile("db/db.json", "utf8", (err, data) => {
-
         if (err) throw err;
-
         var notes = JSON.parse(data);
 
-
     //API routes
-    
+    //This is for the route setup of API/notes
         app.get("/api/notes", function(req, res) {
+            //db.json file and return all saved notes as json
             res.json(notes);
         });
 
-
+    //This is the setup of the API/notes posting route
         app.post("/api/notes", function(req, res){
-
+        //here a new note is received and adds it to db.json, then returns the new note
             let newNote = req.body;
             notes.push(newNote);
             updateDb();
             return console.log("New Note Added: " +newNote.title);
         });
 
+    //here the note is retrieved by a specific id    
         app.get("/api/notes/:id", function(req, res){
 
             res.json(notes[req.params.id]);
         });
 
-
+    // This deletes a note with a specific id    
         app.get("/notes", function(req, res) {
             notes.splice(req.params.id, 1);
             updateDb();
