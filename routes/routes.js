5147c7a3f1dc1085ebef6765c4564f8c6389c445
jeanhern81 <jeanhern81@gmail.com>
2020-07-21 13:57:40 -0700

    started coding the routes for the api

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,26 @@
 const fs = require("fs");
 const path = require("path");
+
+module.exports = app => {
+
+    //Variables for the notes
+
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+
+
+    //API routes
+    
+        app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
+
+
+
+    })
+
+
+}
